ad32309c13268caae670a92ead23e84bbfd1d867
netelli <natalli.dev@gmail.com>
2020-01-27 17:31:44 +0200

    Add basic POST/PUT/DELETE mapping; Use different params for Get requests;

--- src/main/java/com/apps/ws/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/apps/ws/mobileappws/ui/controller/UserController.java
@@ -1,4 +1,47 @@
 package com.apps.ws.mobileappws.ui.controller;
 
+import com.apps.ws.mobileappws.ui.model.response.UserRest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users")
 public class UserController {
+
+    @GetMapping(path = "/{userId}",
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
+        UserRest userRest = new UserRest();
+        userRest.setFirstName("Muddy");
+        userRest.setLastName("Waters");
+        userRest.setEmail("muddy@mail.com");
+
+        return new ResponseEntity<UserRest>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+                           @RequestParam(value = "limit", defaultValue = "50") int limit,
+                           @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+        return "get user was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "delete user was called";
+    }
 }
